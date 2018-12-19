fdcf7c81caa67703d1c3c6168e614802100412c6
RussellG <r.g.levasseur@gmail.com>
2018-12-19 18:12:48 -0500

    Add files via upload

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -1,77 +1,87 @@
 const Discord=require('discord.js');
 
 module.exports.run = async (MAIN, message, args, prefix) => {
-  switch(args[0]){
 
-    // HELP COMMAND OR NO ARGS GIVEN
-    case undefined:
-    case 'help':
-      let availableRewards = MAIN.qConfig.Quest_Rewards.toString().replace(/,/g,'\n');
-      let pokemonHelp = new Discord.RichEmbed().setColor('00ff00')
-        .addField('Pokémon Subscription Command:', '`'+prefix+'pokemon [Pokémon Name]`\nThis command begins the filter setting process.')
-        .setFooter('Type \'Cancel\' to cancel the subscription process.');
-      return message.channel.send(pokemonHelp).then(m => m.delete(60000)).catch(console.error);
+  // DECLARE VARIABLES
+  let pokemon_sub = '', subscriptions = '', nickname = '';
 
-    // PAUSE POKEMON SUBSCRIPTIONS
-    case 'pause':
-      MAIN.database.query("UPDATE pokebot.users SET pokemon_paused = ? WHERE user_id = ?", ['PAUSED',message.member.id], function (error, user, fields) {
-        if(error){ return message.reply('There has been an error, please contact an Admin to fix.').then(m => m.delete(5000)).catch(console.error); }
-        else{ return message.reply('Your Pokémon subscriptions are now `PAUSED`.').then(m => m.delete(5000)).catch(console.error); }
-      }); break;
+  // GET USER NICKNAME
+  if(message.member.nickname){ nickname = message.member.nickname; } else{ nickname = message.member.user.username; }
 
-    // RESUME POKEMON SUBSCRIPTIONS
-    case 'resume':
-      MAIN.database.query("UPDATE pokebot.users SET pokemon_paused = ? WHERE user_id = ?", ['ACTIVE',message.member.id], function (error, user, fields) {
-        if(error){ return message.reply('There has been an error, please contact an Admin to fix.').then(m => m.delete(5000)).catch(console.error); }
-        else{ return message.reply('Your Pokémon subscriptions are now `ACTIVE`.').then(m => m.delete(5000)).catch(console.error); }
-      }); break;
+  // DEFINE THE SUBSCRIPTION
+  let sub = {};
+  let action = await action_collector(nickname,message, undefined,'Names are not case-sensitive.');
+  switch(action){
+    case 'create':
 
-    // CHECK FOR POKEMON AND INITIATE COLLECTOR SEQUENCE
-    default:
-      for(let p=1;p<723;p++){
-        if(args[0].toLowerCase() == MAIN.pokemon[p].name.toLowerCase()){
-          createSubscription(MAIN, message, p); break;
-        }
-        else if(p==722){
-          return message.reply('That doesn\'t appear to be a valid Pokémon name. Please check the spelling and try again.').then(m => m.delete(5000)).catch(console.error);
-        }
-      }
+    case 'remove':
+
+    case 'modify':
   }
-}
-
-async function createSubscription(MAIN, message, num){
-
-  // DEFINE VARIABLES
-  var pokemonName = MAIN.pokemon[num].name;
-
-  let min_cp = await sub_collector('Minimum CP',message,pokemonName,'Please respond with a number greater than 0 or \'All\'.');
-  let min_iv = await sub_collector('Minimum IV',message,pokemonName,'Please respond with a IV number between 0 and 100, specify minimum Atk/Def/Sta (15/14/13) Values or type \'All\'.');
-  let min_lvl = await sub_collector('Minimum Level',message,pokemonName,'Please respond with a value between 0 and 35 or type \'All\'.');
-  let max_lvl = await sub_collector('Maximum Level',message,pokemonName,'Please respond with a value between 0 and 35 or type \'All\'.');
-  let gender = await sub_collector('Gender',message,pokemonName,'Please respond with \'Male\' or \'Female\' or type \'All\'.');
-
-
 
 }
 
-function sub_collector(type,message,pokemon,requirements){
+function sub_collector(type,nickname,message,pokemon,requirements){
   return new Promise(function(resolve, reject) {
 
-    let timeout = true;
+    // DELCARE VARIABLES
+    let timeout = true, instruction = '';
 
     // DEFINE COLLECTOR AND FILTER
     const filter = cMessage => cMessage.member.id==message.member.id;
     const collector = message.channel.createMessageCollector(filter, { time: 120000 });
 
-    let initiateSub=new Discord.RichEmbed().setColor('00ff00')
-      .setAuthor(MAIN.pokemon[p].name+' Subscription Setup Initiated for '+message.member.user.tag, message.member.user.displayAvatarURL)
-      .setDescription(message.member+', what '+type+' would like you like to set for '+pokemon+' Notifications?')
-      .setFooter(requirements);
-    message.channel.send(initiateSub).catch(console.error).then( msg => {
+    if(type == 'Name'){
+      instruction = new Discord.RichEmbed().setColor('00ff00')
+        .setAuthor(nickname, message.member.user.displayAvatarURL)
+        .setTitle('Pokémon Subscription Start')
+        .setDescription('What Pokémon would you like to Subscribe to?')
+        .setFooter(requirements);
+    }
+    else{
+      instruction = new Discord.RichEmbed().setColor('00ff00')
+        .setAuthor(nickname, message.member.user.displayAvatarURL)
+        .setTitle(pokemon+' Subscription Setup')
+        .setDescription('What **'+type+'** would like you like to set for **'+pokemon+'** Notifications?')
+        .setFooter(requirements);
+    }
+
+    let completeSub = new Discord.RichEmbed().setColor('00ff00')
+      .setAuthor(nickname, message.member.user.displayAvatarURL)
+      .setTitle(pokemon+' Subscription Complete!');
+
+    let timeoutSub = new Discord.RichEmbed().setColor('00ff00')
+      .setAuthor(nickname, message.member.user.displayAvatarURL)
+      .setTitle(pokemon+' Subscription Has Timed Out!')
+      .setFooter('You can start a new sub with the \'pokemon\' command.');
+
+    message.channel.send(instruction).catch(console.error).then( msg => {
+
+      let input = '';
 
       // FILTER COLLECT EVENT
       collector.on('collect', message => {
+
+        let timeoutSub = new Discord.RichEmbed().setColor('00ff00')
+          .setAuthor(nickname, message.member.user.displayAvatarURL)
+          .setTitle();
+
         switch(true){
+
+          case message.content.toLowerCase() == 'cancel':
+            message.reply('Cancelling Sub.')
+              .then(m => { m.delete(5000); msg.delete(); return; }).catch(console.error);
+
+          // POKEMON NAME
+          case type.indexOf('Name')>=0:
+            for(let p = 1;p < 723;p++){
+              if(args[0].toLowerCase() == MAIN.pokemon[p].name.toLowerCase()){
+                msg.delete(); resolve(message.content);  collector.stop('complete'); break;
+              }
+              else if(p == 722){
+                message.reply('That doesn\'t appear to be a valid Pokémon name. Please check the spelling and try again.').then(m => m.delete(5000)).catch(console.error);
+              }
+            }
 
           // CP CONFIGURATION
           case type.indexOf('CP')>=0:
@@ -108,20 +118,64 @@ function sub_collector(type,message,pokemon,requirements){
 
           // GENDER CONFIGURATION
           case type.indexOf('Gender')>=0:
-          if(message.content.toLowerCase() == 'all' || message.content.toLowerCase() == 'male' || message.content.toLowerCase() == 'female'){
-            message.reply('`'+type+'` setting for `'+pokemon+'` has been set to `'+message.content+'`.')
-              .then(m => { m.delete(5000); resolve(message.content); collector.stop(); msg.delete();  }).catch(console.error);
-          }
-          else{ message.reply('Invalid Input. '+requirements).then(m => m.delete(5000)).catch(console.error); msg.delete(); } break;
+            if(message.content.toLowerCase() == 'all' || message.content.toLowerCase() == 'male' || message.content.toLowerCase() == 'female'){
+              msg.delete(); collector.stop('complete'); resolve(message.content);
+            }
+            else{ message.reply('Invalid Input. '+requirements).then(m => m.delete(5000)).catch(console.error); msg.delete(); } break;
 
-          case message.content.toLowerCase == 'cancel':
-            message.reply('Subscription has been cancelled. Nothing was saved to the database.')
-              .then(m => { m.delete(5000); resolve(message.content); collector.stop(); msg.delete();  }).catch(console.error);
+
+          case type.indexOf('confirm')>=0:
+            if(message.content.toLowerCase() == 'all' || message.content.toLowerCase() == 'male' || message.content.toLowerCase() == 'female'){
+              msg.delete(); collector.stop('complete'); resolve(message.content);
+            }
+            else{ message.reply('Invalid Input. '+requirements).then(m => m.delete(5000)).catch(console.error); msg.delete(); } break;
+
+
         }
+        // collector.on('collect', (collected,reason) => {
+        //   if(reason){
+        //     console.log(reason);
+        //   }
+        // });
       });
-
     });
+  });
+}
 
+async function subscription_create(){
 
+  sub.name = await sub_collector('Name',nickname,message, undefined,'Names are not case-sensitive.');
+  sub.min_cp = await sub_collector('Minimum CP',nickname,message,sub.name,'Please respond with a number greater than 0 or \'All\'. Type \'Cancel\' to Stop.');
+  sub.max_cp = await sub_collector('Maximum CP',nickname,message,sub.name,'Please respond with a number greater than 0 or \'All\'. Type \'Cancel\' to Stop.');
+  sub.min_iv = await sub_collector('Minimum IV',nickname,message,sub.name,'Please respond with a IV number between 0 and 100, specify minimum Atk/Def/Sta (15/14/13) Values or type \'All\'. Type \'Cancel\' to Stop.');
+  sub.min_lvl = await sub_collector('Minimum Level',nickname,message,sub.name,'Please respond with a value between 0 and 35 or type \'All\'. Type \'Cancel\' to Stop.');
+  sub.max_lvl = await sub_collector('Maximum Level',nickname,message,sub.name,'Please respond with a value between 0 and 35 or type \'All\'. Type \'Cancel\' to Stop.');
+  sub.gender = await sub_collector('Gender',nickname,message,sub.name,'Please respond with \'Male\' or \'Female\' or type \'All\'.');
+  let confirm = await sub_collector('Confirm',nickname,message,sub.name,'Type \'Yes\' or \'No\'. Subscription will be saved.');
+
+  MAIN.database.query("SELECT * FROM pokebot.users WHERE user_id = ?", [message.member.id], function (error, user, fields) {
+    console.log(user[0]);
+    if(!user[0].pokemon){
+      console.log('Creating new Sub');
+      pokemon_sub = {};
+      pokemon_sub.subscriptions = [];
+      pokemon_sub.subscriptions.push(sub);
+    }
+    else{
+      console.log('Found existing Sub');
+      pokemon_sub = JSON.parse(user[0].pokemon);
+      pokemon_sub.subscriptions.push(sub);
+    }
+    console.log(pokemon_sub);
+    MAIN.database.query("UPDATE pokebot.users SET pokemon = ? WHERE user_id = ?", [pokemon_sub,message.member.id], function (error, user, fields) {
+      if(error){ return message.reply('There has been an error, please contact an Admin to fix.').then(m => m.delete(10000)).catch(console.error); }
+      else{
+        let subscription_success = new Discord.RichEmbed()
+          .setColor('00ff00')
+          .addField('Pokémon Subscription Command:', '`'+prefix+'pokemon [Pokémon Name]`\nThis command begins the filter setting process.')
+          .setFooter('Type \'Cancel\' to cancel the subscription process.');
+        return message.channel.send(subscription_success).then(m => m.delete(60000)).catch(console.error);
+      }
+    });
   });
 }
