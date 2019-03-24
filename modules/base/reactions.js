b7563cc9e52d7c42df85c748966097dbc34a59f3
Brian Whisler <shindekokoro@Brians-MacBook.local>
2019-03-24 06:53:03 -0600

    ugh

--- modules/base/reactions.js
+++ modules/base/reactions.js
@@ -40,11 +40,6 @@ reactions.run = (MAIN, event) => {
                 MAIN.channels.get(record[0].raid_channel).send(member+' has shown interest in the raid! Make sure to coordinate a start time.').catch(console.error);
               } else{
 
-                // UPDATE SQL RECORD
-                MAIN.pdb.query(`UPDATE active_raids SET active = ?, channel_id = ?, initiated_by = ?, raid_channel = ?, created = ? WHERE gym_id = ?`, ['true', channel.id, member.id, channel_id, moment().unix(), gym_id], function (error, raids, fields) {
-                  if(error){ console.error(error); }
-                });
-
                 // SET THE CHANNEL NAME
                 let channel_name = record[0].boss_name+'_'+record[0].gym_name
 
@@ -73,8 +68,12 @@ reactions.run = (MAIN, event) => {
                     let mention = '<@&'+discord.raid_role+'> '
                     if (mention == "<@&> "){ mention = '' }
                     new_channel.send(mention+member+' has shown interest in a raid! Make sure to coordinate a start time.', channel_embed).catch(console.error);
-
                     new_channel.lockPermissions();
+
+                    // UPDATE SQL RECORD
+                    MAIN.pdb.query(`UPDATE active_raids SET active = ?, channel_id = ?, initiated_by = ?, raid_channel = ?, created = ? WHERE gym_id = ?`, ['true', channel.id, member.id, channel_id, moment().unix(), gym_id], function (error, raids, fields) {
+                      if(error){ console.error(error); }
+                    });
                   });
                 });
               }
