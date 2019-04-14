3de5ccf81fbab53669e6ef3eba0a14ee56a48336
Brian Whisler <brian.whisler@gmail.com>
2019-04-14 15:10:01 -0600

    [0]

--- modules/embeds/pokemon.js
+++ modules/embeds/pokemon.js
@@ -89,12 +89,12 @@ module.exports.run = async (MAIN, has_iv, target, sighting, internal_value, time
     if (verified == MAIN.emotes.yellowQuestion) {
       MAIN.rdmdb.query('SELECT * FROM pokemon WHERE id = ?', [sighting.encounter_id], function (error, record, fields) {
         if(error){ console.error(error); }
-        console.log(record.expire_timestamp_verified+'<db-webhook>'+sighting.encounter_id)
-        if (record.expire_timestamp_verified == 1) {
+        console.log(record[0].expire_timestamp_verified+'<db-webhook>'+sighting.encounter_id)
+        if (record[0].expire_timestamp_verified == 1) {
           console.log('DESPAWN is verified');
-          hide_time = MAIN.Bot_Time(record.expire_timestamp, '1', timezone);
-          hide_mins = Math.floor((record.expire_timestamp-(time_now/1000))/60);
-          hide_secs = Math.floor((record.expire_timestamp-(time_now/1000)) - (hide_mins*60));
+          hide_time = MAIN.Bot_Time(record[0].expire_timestamp, '1', timezone);
+          hide_mins = Math.floor((record[0].expire_timestamp-(time_now/1000))/60);
+          hide_secs = Math.floor((record[0].expire_timestamp-(time_now/1000)) - (hide_mins*60));
           verified = MAIN.emotes.checkYes;
         } else {console.log('DESPAWN is not verified');}
       });
