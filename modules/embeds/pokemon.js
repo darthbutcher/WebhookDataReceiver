365255cc1fe27c4699a89ca9acc6153f9ee7c85e
Brian Whisler <brian.whisler@gmail.com>
2019-04-14 14:35:12 -0600

    2

--- modules/embeds/pokemon.js
+++ modules/embeds/pokemon.js
@@ -52,12 +52,12 @@ module.exports.run = async (MAIN, has_iv, target, sighting, internal_value, time
     MAIN.rdmdb.query('SELECT * FROM pokemon WHERE id = ?', [encounter_id], function (error, record, fields) {
       if(error){ console.error(error); }
       if (record.expire_timestamp_verified == 1) {
-        console.log('DESPAWN is verified')
-        hide_time = await MAIN.Bot_Time(record.expire_timestamp, '1', timezone);
+        console.log('DESPAWN is verified');
+        hide_time = MAIN.Bot_Time(record.expire_timestamp, '1', timezone);
         hide_mins = Math.floor((record.expire_timestamp-(time_now/1000))/60);
         hide_secs = Math.floor((record.expire_timestamp-(time_now/1000)) - (hide_mins*60));
         verified = MAIN.emotes.checkYes;
-      }
+      } else {console.log('DESPAWN is not verified');}
     });
   }
 
