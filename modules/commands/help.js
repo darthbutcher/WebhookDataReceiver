9d17bbe9cf8742658e0b6c7ea5038ec94b3b7cb3
darthbutcher <45851918+darthbutcher@users.noreply.github.com>
2019-05-31 02:39:01 -0400

    Add general commands to help
    This adds and separates new general commands from subscription commands when viewing the help embed.

--- modules/commands/help.js
+++ modules/commands/help.js
@@ -7,9 +7,14 @@ module.exports.run = async (MAIN, message, prefix, discord) => {
   if(MAIN.config.POKEMON.Subscriptions == 'ENABLED'){ quest_help.addField('`'+prefix+'pokemon`  |  `'+prefix+'p`', 'Initializes Pokemon Subscription Options.', false); }
   if(MAIN.config.QUEST.Subscriptions == 'ENABLED'){ quest_help.addField('`'+prefix+'quest` |  `'+prefix+'q` ', 'Initializes Quest Subscription Options.', false); }
   if(MAIN.config.RAID.Subscriptions == 'ENABLED'){ quest_help.addField('`'+prefix+'raid` |  `'+prefix+'r` ', 'Initializes Raid Subscription Options.', false); }
-  if(MAIN.config.pmsfDB.Search == 'ENABLED'){ quest_help.addField('`'+prefix+'nest` |  `'+prefix+'n` ', 'Initializes Nest Search.', false); }
-  quest_help.addField('`'+prefix+'stats` |  `'+prefix+'s` ', 'Initializes Pokémon stats lookup.', false);
   quest_help.addField('`'+prefix+'area`', 'Shows Area subscription options.', false)
   quest_help.addField('`'+prefix+'pause` | `'+prefix+'resume`', 'Pause or Resume ALL subscription alerts.', false);
+  quest_help.addBlankField(true)
+  if(MAIN.config.pmsfDB.Search == 'ENABLED'){ quest_help.addField('`'+prefix+'nest` |  `'+prefix+'n` ', 'Initializes Nest Search.', false); }
+  quest_help.addField('`'+prefix+'stats` |  `'+prefix+'s` ', 'Initializes Pokémon stats lookup.', false);
+  quest_help.addField('`'+prefix+'cp` ', 'Initializes perfect Pokémon CP string lookup.', false);
+  quest_help.addField('`'+prefix+'raidcp` ', 'Initializes raid Pokemon top CPs table.', false);
+  quest_help.addField('`'+prefix+'questcp` ', 'Initializes quest Pokemon top CPs table.', false);
+  quest_help.addField('`'+prefix+'dex` ', 'Initializes Pokedex search.', false);
   return message.channel.send(quest_help).catch(console.error);
 }
